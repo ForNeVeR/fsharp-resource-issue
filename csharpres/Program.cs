bbd7d9c839cca34c3aaa692d344766627b26aa2a
Friedrich von Never <friedrich@fornever.me>
2017-12-17 17:04:49 +0700

    Initial commit

---
+++ csharpres/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace csharpres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var resource in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+            {
+                Console.WriteLine(resource);
+            }
+        }
+    }
+}
---
+++ fsharpres/Program.fs
@@ -0,0 +1,7 @@
+﻿open System.Reflection
+
+[<EntryPoint>]
+let main _ =
+    Assembly.GetExecutingAssembly().GetManifestResourceNames()
+    |> Array.iter (printfn "%s")
+    0
